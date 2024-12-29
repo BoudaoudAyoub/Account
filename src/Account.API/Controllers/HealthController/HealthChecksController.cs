e7286c284cc1ff8c3e206d72314f32743cf17ad2
Ayoub Boudaoud <ayoub.boudaoud@terabyte-software.com>
2024-12-29 21:57:26 +0100

    Change launch settings urls and added the health check conrtoller

---
+++ src/Account.API/Controllers/HealthController/HealthChecksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Account.API.Controllers.HealthController;
+
+[ApiController]
+[Route("[Controller]")]
+public class HealthChecksController(ILogger<HealthChecksController> logger) : ControllerBase
+{
+    public readonly ILogger<HealthChecksController> _logger = logger;
+
+    [HttpGet]
+    public async Task<object> GetHelloWorld() => await Task.FromResult("Hello World");
+}
